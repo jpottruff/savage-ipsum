00292bdd7d065d6b7c3507903733e5127621d6e1
Jeff Pottruff <jeffpottruff@gmail.com>
2021-03-02 23:21:06 -0500

    ADD purge path for builds

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: ['./views/**/*.handlebars'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {

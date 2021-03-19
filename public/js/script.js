6887e18ac358196f792dc17eebc092693d92b54a
Jeff Pottruff <jeffpottruff@gmail.com>
2021-03-18 21:33:24 -0400

    ADD copy confirmation message

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,23 @@
 function copyToClipboard() {
-  // Grab the text; add 2 new lines for formatting
+  const btn = document.getElementById('copy-btn');
+  const msgText = document.getElementById('copied-msg-text');
+  const copyMsg = document.getElementById('copied-msg');
+
+  btn.disabled = true;
+  copyText();
+  msgText.innerText = getMsgText();
+  copyMsg.classList.remove('invisible');
+  copyMsg.classList.add('animate-swell-single');
+
+  setTimeout(() => {
+    copyMsg.classList.add('invisible');
+    copyMsg.classList.remove('animate-swell-single');
+    btn.disabled = false;
+  }, 1000);
+}
+
+function copyText() {
+  // Grab the text; Add 2 new lines for formatting
   const paragraphNodes = document.querySelectorAll('p');
   const formattedText = Array.from(paragraphNodes)
     .map((p) => p.innerText)
@@ -17,4 +35,8 @@ function copyToClipboard() {
   el.select();
   document.execCommand('copy');
   document.body.removeChild(el);
+}
+
+function getMsgText() {
+  return Math.random() < 0.5 ? 'Ohhhh yeeeeah!!!' : 'Copied!!!';
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 module.exports = {
-  purge: ['./views/**/*.handlebars'],
+  purge: [
+    './views/**/*.handlebars', 
+    './public/js/*.js' // for dynamically set classes
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      animation: {
+        'ping-slow': 'ping 2s cubic-bezier(0, 0, 0.2, 1) infinite',
+        'swell-single': 'ping 3s cubic-bezier(0, 0, 0.2, 1) 1',
+      },
       fontFamily: {
         marker: ['Permanent Marker', 'cursive'],
         'sans-alt': ['montserrat', 'sans-serif'],
